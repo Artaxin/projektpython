bd87a294572483c0888cc85f5a5823ae38d92220
Artaxin <jakub.domoslawski@gmail.com>
2021-06-13 16:04:23 +0200

    Add files via upload

--- 3gry.py
+++ 3gry.py
@@ -2,29 +2,42 @@ import random
 
 
 print("Witam w symulatorze gier!")
-
-# gry do wyboru
 games = ["Rzut kostką", "Rzut monetą", "Losowanie liczb"]
 x, y, z = games
 
 
-# wstęp
-print("Dostępne gry to " + x + " , " + y + " , " + z + ". Którą wybierasz? ")
-print("Menu: gra 1 to "+x+", gra 2 to "+y+", gra 3 to "+z)
+
+print("Witaj w symulatorze. Dostępne gry to " + x + " , " + y + " , " + z + ".")
+print(" ")
 
 
 
-choice = input("Wybierz 1, 2 lub 3 :")
+class Gry:
+    def __init__(self, game):
+        self.game = game
+        
+
+g1 = Gry("Rzut Kostką")
+g2 = Gry("Rzut monetą")
+g3 = Gry("Losowanie liczb")
+
+
+
+print("Menu: Gra nr 1 to "+g1.game+", gra nr 2 to "+g2.game+", gra nr 3 to "+g3.game+". Którą wybierasz?")
+
+
+
+choice = input("Wybierz 1, 2 lub 3 by wybrać grę. Zero by wyjść z programu :")
 
 
 if choice == '1':
-    print("Wybierz ilość pól na kostce")
+    print("Wybierz ilość pól na kostce :.")
     pola = int(input())
-    print ("Rzucono kostką, wypadło: " + str(random.randint(1, pola)))
-        
+    print ("w grze "+g1.game+" wypadło: " + str(random.randint(1, pola)))
+   
 
 elif choice == '2':
-    print ("Rzucono monetą, wynik to " + ("orzeł" if random.randint(0, 1) == 0 else "reszka"))
+    print ("w grze "+g2.game+" wynik to " + ("Orzeł" if random.randint(0, 1) == 0 else "Reszka"))
         
 
 elif choice == '3':
@@ -33,13 +46,16 @@ elif choice == '3':
     print("Podaj górny zakres liczb")
     zakres = int(input())
     los = list(range(1, zakres))
-    print("Wylosowane liczby to:")
+    print("w grze "+g3.game+" wylosowane liczby to:")
     for i in range(0, liczby):
         
             liczba = random.choice(los)
             los.remove(liczba)
             print(liczba)
-        
+
+elif choice == '0':
+    print ("Do zobaczenia!!!")
+
 
 else :
     print("podano złą liczbę") 
